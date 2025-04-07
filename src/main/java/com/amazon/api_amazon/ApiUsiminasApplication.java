c1d1591b5e2d21e5988dac54ad9a1fcd74de8428
Matheus Cristian <matheuscristianrg@gmail.com>
2025-04-07 16:20:16 -0300

    Primeiro Commit

---
+++ src/main/java/com/amazon/api_amazon/ApiUsiminasApplication.java
@@ -0,0 +1,13 @@
+package com.amazon.api_amazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUsiminasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUsiminasApplication.class, args);
+	}
+
+}
