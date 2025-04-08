bcd7086eaab2b1f82485e3b8f337eda27f466319
Matheus Cristian <matheuscristianrg@gmail.com>
2025-04-08 13:42:15 -0300

    Começando a integrar o JDBC

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-https://prod.liveshare.vsengsaas.visualstudio.com/join?E777BCB0CC97EEC55D9930972E674ADAA74B
+https://prod.liveshare.vsengsaas.visualstudio.com/join?5589120DBB99795D2D769AFA142DD429DCD3
--- src/main/java/com/nozama/api_nozama/ApiNozamaApplication.java
+++ src/main/java/com/nozama/api_nozama/ApiNozamaApplication.java
@@ -8,6 +8,12 @@ public class ApiNozamaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiNozamaApplication.class, args);
+
+		String url = "jdbc:mysql://localhost:3306/your_database_name";
+        String username = "your_username";
+        String password = "your_password";
+
+		
 	}
 
 }
