f5824638af97031d697b15da658ee43ee3b4734d
Matheus Cristian <matheuscristianrg@gmail.com>
2025-04-08 14:31:48 -0300

    Termina adicionar conexão com banco de dados

--- src/main/java/com/nozama/api_nozama/ApiNozamaApplication.java
+++ src/main/java/com/nozama/api_nozama/ApiNozamaApplication.java
@@ -1,19 +1,32 @@
 package com.nozama.api_nozama;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiNozamaApplication {
 
+	private static Connection conn = null;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ApiNozamaApplication.class, args);
 
-		String url = "jdbc:mysql://localhost:3306/your_database_name";
-        String username = "your_username";
-        String password = "your_password";
+		String url = "jdbc:mysql://localhost:3306/Amazon";
+		String username = "root";
+		String password = null;
 
-		
+		try {
+			conn = DriverManager.getConnection(url, username, password);
+			System.out.println("Connected to the database!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
+	public static Connection getConn() {
+		return conn;
+	}
 }
